ac92eb1da1338a0519d4b1334bb22fbebf08aa45
Karthick-sketch <karthick246.kh@outlook.com>
2024-03-22 22:06:47 +0530

    Change FileUploadStatusController request paths

--- customer-record-service/src/main/java/com/customerrecordsmanagement/fileuploadstatus/FileUploadStatusController.java
+++ customer-record-service/src/main/java/com/customerrecordsmanagement/fileuploadstatus/FileUploadStatusController.java
@@ -17,7 +17,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("/upload-status/account")
+@RequestMapping("/upload-status/account/{accountId}")
 public class FileUploadStatusController {
     private final JobLauncher exportJobLauncher;
     private final Job exportJob;
@@ -36,26 +36,26 @@ public class FileUploadStatusController {
         this.importJob = importJob;
     }
 
-    @GetMapping("/{accountId}/all")
+    @GetMapping("/all")
     public ResponseEntity<List<FileUploadStatus>> getAllFileUploadStatus(@PathVariable long accountId) {
         return new ResponseEntity<>(fileUploadStatusService.fetchFileUploadStatusByAccountId(accountId), HttpStatus.OK);
     }
 
-    @GetMapping("/{accountId}/id/{id}")
+    @GetMapping("/id/{id}")
     public ResponseEntity<FileUploadStatus> getFileUploadStatusById(@PathVariable long accountId, long id) {
         return new ResponseEntity<>(fileUploadStatusService.fetchFileUploadStatusByIdAndAccountId(id, accountId), HttpStatus.OK);
     }
 
-    @PostMapping("/{accountId}/upload-csv")
+    @PostMapping("/upload-csv")
     public ResponseEntity<FileUploadStatusDTO> uploadCsvFile(@PathVariable long accountId, @RequestPart("file") MultipartFile file) {
         return new ResponseEntity<>(fileUploadStatusService.uploadCsvFile(accountId, file), HttpStatus.OK);
     }
 
-    @GetMapping("/{accountId}/startExportJob")
+    @GetMapping("/export-csv")
     public ResponseEntity<Resource> startExportJob(@PathVariable long accountId) throws Exception {
         File file = new File("src/main/resources/exports/" + accountId + "-customer-records-" + LocalDateTime.now() + ".csv");
 
-        System.out.println("Batch started ............");
+        System.out.println("Export batch started ............");
         JobParameters jobParameter = new JobParametersBuilder()
                 .addLong("accountId", accountId)
                 .addString("filePath", file.getAbsolutePath())
@@ -65,7 +65,7 @@ public class FileUploadStatusController {
         while (jobExecution.isRunning()) {
             System.out.println("......");
         }
-        System.out.println("Batch status : " + jobExecution.getStatus());
+        System.out.println("Export batch status : " + jobExecution.getStatus());
 
         Resource resource = new PathResource(file.getAbsolutePath());
         return ResponseEntity.ok()
@@ -74,11 +74,11 @@ public class FileUploadStatusController {
                 .body(resource);
     }
 
-    @PostMapping("/{accountId}/startImportJob")
+    @PostMapping("/startImportJob")
     public ResponseEntity<HttpStatus> startImportJob(@PathVariable long accountId) throws Exception {
         String filePath = fileUploadStatusService.getFilePath("1-contact.csv", true);
 
-        System.out.println("Batch started ............");
+        System.out.println("Import batch started ............");
         JobParameters jobParameter = new JobParametersBuilder()
                 .addLong("accountId", accountId)
                 .addString("filePath", filePath)
@@ -88,7 +88,7 @@ public class FileUploadStatusController {
         while (jobExecution.isRunning()) {
             System.out.println("......");
         }
-        System.out.println("Batch status : " + jobExecution.getStatus());
+        System.out.println("Import batch status : " + jobExecution.getStatus());
 
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
